5cd0de7ca9f6a6e9c930d57bd7fc6cb5c9529dc0
naeunk1m <08enmik@gmail.com>
2023-08-04 14:20:25 +0900

    20230804

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,27 +104,27 @@ gsap.to(".left", {
 
 // sc-possibility
 
-const horizontal = document.querySelector("#horizontal");
-const sections = gsap.utils.toArray("#horizontal .scroll");
+function horizontalScroll() {
+    const sections = gsap.utils.toArray(".scroll");
+    const scrollDistance = window.innerWidth;
 
-gsap.to( sections, {
-    xPercent: -100,
-    ease: "none",
-    scrollTrigger: {
-        trigger: horizontal,
-        start: "top top",
-        end: () =>  "+=" + (horizontal.offsetWidth - innerWidth),
-        pin: true,
-        scrub: 1,
-        snap: {
-            snapTo: 1 / (sections.length - 1),
-            inertia: false,
-            duration: {min: 0.1, max: 0.1}
-        },
-        invalidateOnRefresh: true,
-        anticipatePin: 1
-    }
-});
+    gsap.to(sections, {
+        x:() => -ScrollTrigger.progress * scrollDistance,
+        ease: "none", 
+        scrollTrigger: {
+            trigger: ".sc-showcase2",
+            start: "top top",
+            end: "bottom", 
+            scrub: true, 
+            markers: true,
+        }
+    })
+}
+
+horizontalScroll();
+
+window.addEventListener("resize", horizontalScroll);
+
 
 // sc-possibility
 
